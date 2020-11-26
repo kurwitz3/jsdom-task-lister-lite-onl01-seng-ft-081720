d1441790beeefa677808a6b3213073e83ee1411c
kurwitz3 <kurwitz3@aol.com>
2020-11-25 20:15:37 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  loadFormlistener()
 });
+
+function loadFormlistener(){
+  const loadForm = document.getElementById('create-task-form')
+
+  loadForm.addEventListener("submit", function(event){
+    event.preventDefault()
+   const task =  getInfo(event)
+   const htmlPost = postHtml(task)
+   attachPost(htmlPost)
+  })
+
+  function getInfo(event){
+   return {
+    input: event.target.querySelector("#new-task-description").value
+   }
+  
+  }
+  function postHtml(post){
+    return `
+  
+       <li>${post.input}</li>
+ 
+  `
+  }
+  const attachPost = function(post){
+    document.getElementById('tasks').innerHTML += post
+  } 
+
+}
